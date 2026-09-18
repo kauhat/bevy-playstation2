0702bfc3acdac797aba770e4afe2edf0179f32a7
Jack Fletcher <jackowenfletcher@gmail.com>
2026-09-18 21:14:26 +0000

    download ps2dev toolchain binaries

--- flake.nix
+++ flake.nix
@@ -5,10 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixgl.url = "github:nix-community/nixGL";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    ps2sdk-src = {
-      url = "github:ps2dev/ps2sdk";
-      flake = false;
-    };
   };
 
   outputs = {
@@ -16,7 +12,6 @@
     nixpkgs,
     nixgl,
     rust-overlay,
-    ps2sdk-src,
   }: let
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -30,23 +25,56 @@
         ];
       };
 
-crossPkgs = import nixpkgs {
-        inherit system;
-        crossSystem = {
-          config = "mips64el-none-elf";
-          gcc.arch = "mips3"; 
+      # Exact SHA256 hashes and asset names for ps2dev v2.0.0
+      ps2devSrc =
+        {
+          "x86_64-linux" = {
+            asset = "ubuntu-latest";
+            hash = "sha256:c8e5dedccf62084d476894e88cd0451b57f1ed288e741eb3c1263010aaffc028";
+          };
+          "aarch64-linux" = {
+            asset = "ubuntu-24.04-arm";
+            hash = "sha256:ed69330f235d421754196acf866f8590e5f92e8b92571f1c7e05e853900e7622";
+          };
+          "x86_64-darwin" = {
+            asset = "macos-15-intel";
+            hash = "sha256:833d2dc1e3cbfa2091047e4539e1bd0bd2836668f886a1c8a75e6e09deb56bef";
+          };
+          "aarch64-darwin" = {
+            asset = "macos-latest";
+            hash = "sha256:6f6df659d2d49738f21f53a53c361a74182d0e4c2495e31e76972ff818bc21cb";
+          };
+        }.${
+          system
         };
-        overlays = [
-          (final: prev: {
-            # Use overrideAttrs to pass the flag to the configure script
-            newlib = prev.newlib.overrideAttrs (old: {
-              configureFlags = (old.configureFlags or []) ++ [ "--disable-libgloss" ];
-            });
-          })
+
+      ps2dev = pkgs.stdenv.mkDerivation {
+        pname = "ps2dev";
+        version = "2.0.0";
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/ps2dev/ps2dev/releases/download/v2.0.0/ps2dev-${ps2devSrc.asset}.tar.gz";
+          sha256 = ps2devSrc.hash;
+        };
+
+        # autoPatchelfHook runs on Linux; macOS binaries are already dynamically linked for Mach-O
+        nativeBuildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux [pkgs.autoPatchelfHook];
+
+        # Injected the missing multiprecision math libraries required by GCC
+        buildInputs = [
+          pkgs.zlib
+          pkgs.stdenv.cc.cc.lib
+          pkgs.gmp
+          pkgs.mpfr
+          pkgs.libmpc
         ];
+
+        installPhase = ''
+          mkdir -p $out
+          cp -r * $out/
+        '';
       };
 
-      mipsCC = crossPkgs.stdenv.cc;
       nixGLPkg = nixgl.packages.${system}.nixGLDefault;
 
       rustToolchain = pkgs.rust-bin.nightly.latest.default.override {
@@ -58,42 +86,8 @@ crossPkgs = import nixpkgs {
         cargo = rustToolchain;
         rustc = rustToolchain;
       };
-
-      ps2sdk = pkgs.stdenv.mkDerivation {
-        pname = "ps2sdk";
-        version = "latest";
-        src = ps2sdk-src;
-
-        nativeBuildInputs = [pkgs.gnumake mipsCC];
-
-        patchPhase = ''
-          substituteInPlace ee/Rules.make \
-            --replace-quiet "EE_CFLAGS =" "EE_CFLAGS ?=" || true
-          substituteInPlace Rules.make \
-            --replace-quiet "EE_CFLAGS =" "EE_CFLAGS ?=" || true
-        '';
-
-        buildPhase = ''
-          export PS2SDK=$out
-
-          # The compiler now natively provides <string.h> via our overlaid newlib sysroot
-          export EE_CFLAGS="-mips3 -mgp64 -mlong64 -D_EE -fno-PIC -Wno-error"
-
-          make -j$NIX_BUILD_CORES release \
-            EE_CC=mips64el-none-elf-gcc \
-            EE_CXX=mips64el-none-elf-g++ \
-            EE_AR=mips64el-none-elf-ar \
-            EE_LD=mips64el-none-elf-ld \
-            EE_AS=mips64el-none-elf-as \
-            IOP_CC=mips64el-none-elf-gcc \
-            IOP_AR=mips64el-none-elf-ar \
-            IOP_AS=mips64el-none-elf-as
-        '';
-
-        dontInstall = true;
-      };
     in {
-      inherit pkgs crossPkgs mipsCC nixGLPkg rustToolchain rustPlatform ps2sdk;
+      inherit pkgs nixGLPkg rustToolchain rustPlatform ps2dev;
     });
   in {
     devShells = forAllSystems (system: let
@@ -101,7 +95,7 @@ crossPkgs = import nixpkgs {
     in {
       default = e.pkgs.mkShell {
         buildInputs = [
-          e.mipsCC
+          e.ps2dev
           e.pkgs.gnumake
           e.pkgs.git
           e.pkgs.just
@@ -112,9 +106,11 @@ crossPkgs = import nixpkgs {
         ];
 
         shellHook = ''
-          export PS2SDK="${e.ps2sdk}"
-          export PS2SDK_LIBDIR="${e.ps2sdk}/ee/lib"
-          export PS2SDK_INCDIR="${e.ps2sdk}/ee/include"
+          export PS2SDK=${e.ps2dev}/ps2sdk
+          export GSKIT=${e.ps2dev}/gsKit
+          export PATH=$PATH:${e.ps2dev}/bin:${e.ps2dev}/ee/bin:${e.ps2dev}/iop/bin:${e.ps2dev}/dvp/bin:$PS2SDK/bin
+          export PS2SDK_LIBDIR="$PS2SDK/ee/lib"
+          export PS2SDK_INCDIR="$PS2SDK/ee/include"
           echo "PS2 SDK Build Environment Loaded for ${system}!"
         '';
       };
@@ -137,11 +133,13 @@ crossPkgs = import nixpkgs {
         version = "0.1.0";
         src = ./.;
         cargoLock.lockFile = ./Cargo.lock;
-        nativeBuildInputs = [e.mipsCC];
+        nativeBuildInputs = [e.ps2dev];
         buildPhase = ''
-          export PS2SDK="${e.ps2sdk}"
-          export PS2SDK_LIBDIR="${e.ps2sdk}/ee/lib"
-          export PS2SDK_INCDIR="${e.ps2sdk}/ee/include"
+          export PS2SDK="${e.ps2dev}/ps2sdk"
+          export PS2SDK_LIBDIR="$PS2SDK/ee/lib"
+          export PS2SDK_INCDIR="$PS2SDK/ee/include"
+
+          export PATH=$PATH:${e.ps2dev}/bin:${e.ps2dev}/ee/bin:${e.ps2dev}/iop/bin:${e.ps2dev}/dvp/bin:$PS2SDK/bin
 
           cargo build -Z build-std=core,alloc -Z build-std-features=compiler-builtins-mem -Z json-target-spec --target mipsel-sony-ps2.json --release
         '';
