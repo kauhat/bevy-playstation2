1483a4febbdba52cdce111849b9ace64aebc66a6
Jack Fletcher <jackowenfletcher@gmail.com>
2026-09-21 21:48:51 +0000

    move some linker stuff to cargo config

--- build.rs
+++ build.rs
@@ -10,18 +10,10 @@ fn main() {
     if target_arch == "mips64" {
         println!("It's MIPS! <3");
 
-        // Tell cargo where to search for PS2SDK static libraries (.a)
+        // PS2SDK static libraries...
         println!("cargo:rustc-link-search=native={}/ee/lib", ps2sdk);
+       
+        // PS2SDK linker script...
         println!("cargo:rustc-link-arg=-T{}/ee/startup/linkfile", ps2sdk);
-
-        //
-        println!("cargo:rustc-link-arg=-Wl,--start-group");
-        println!("cargo:rustc-link-arg=-lc");
-        println!("cargo:rustc-link-arg=-lcglue");
-        println!("cargo:rustc-link-arg=-lkernel");
-        println!("cargo:rustc-link-arg=-latomic");
-        // println!("cargo:rustc-link-arg=-lpthread");
-        println!("cargo:rustc-link-arg=-ldebug");
-        println!("cargo:rustc-link-arg=-Wl,--end-group");
     }
 }
