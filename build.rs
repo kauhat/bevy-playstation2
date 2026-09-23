e7a421c1f095c45291a472bee852bcf17205fe3c
Jack <jackowenfletcher@gmail.com>
2026-09-23 23:34:44 +0100

    fix root build script

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::env;
 fn main() {
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
 
-    if target_arch != "mips64" {
+    if target_arch == "mips64" {
         // Get file path from the ps2sdk-sys build script.
         let linkfile = env::var("DEP_PS2SDK_LINKFILE_PATH")
             .expect("Failed to get linkfile path from ps2sdk-sys");
