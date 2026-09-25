087cd79354801d331f24662c9cb1eaf5af031201
Jack Fletcher <jackowenfletcher@gmail.com>
2026-09-25 15:23:08 +0100

    update ps2sdk-sys build script

--- crates/ps2sdk-sys/build.rs
+++ crates/ps2sdk-sys/build.rs
@@ -8,23 +8,20 @@ fn main() {
         return;
     }
 
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-env-changed=PS2SDK");
-
-    // Read $PS2SDK path from environment
     let ps2sdk =
         env::var("PS2SDK").expect("PS2SDK environment variable is required for MIPS builds");
 
-    // PS2SDK static libraries and linker script...
-    println!("cargo:rustc-link-search=native={}/ee/lib", ps2sdk);
-    println!("cargo:rustc-link-arg=-T{}/ee/startup/linkfile", ps2sdk);
+    let libdir = format!("{ps2sdk}/ee/lib");
+    let linkfile = format!("{ps2sdk}/ee/startup/linkfile");
 
-    // Export the linker script to dependents.
-    println!("cargo:linkfile_path=-T{}/ee/startup/linkfile", ps2sdk);
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed=PS2SDK");
 
-    // PS2SDK linker script...
-    // println!("cargo:rustc-link-arg={}/ee/startup/src/crt0.o", ps2sdk);
+    // These apply to ps2sdk-sys itself.
+    println!("cargo:rustc-link-search=native={libdir}");
+    println!("cargo:rustc-link-arg=-T{linkfile}");
 
-    // Dead code elimination
-    // println!("cargo:rustc-link-arg=-Wl,--gc-sections");
+    // These are metadata for the direct dependent.
+    println!("cargo:linkfile_path={linkfile}");
+    println!("cargo:link_search={libdir}");
 }
